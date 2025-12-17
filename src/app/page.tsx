a4e55b0c63dbe54d910a177ca164a184525ab018
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2025-12-17 17:01:11 -0500

    Updates

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import styles from "./page.module.css";
 
-export default function Home() {
+export default function Page() {
   return (
     <main className={styles.wrapper}>
       <div className={styles.card}>
