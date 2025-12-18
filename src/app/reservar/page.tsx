94655c3217df597cc9305ac3b3ec6a4d52d52f35
Jeffer-Andres-Tafurth-Cortes <jeffer.andres.tafurth.cortes@gmail.com>
2025-12-18 09:16:53 -0500

    Updates

--- src/app/reservar/page.tsx
+++ src/app/reservar/page.tsx
@@ -1,1 +1,8 @@
-export {};
+export default function PagoPage() {
+  return (
+    <div>
+      <h1>Página de Pago</h1>
+      <p>Bienvenido a la página de pago. Aquí puedes completar tu compra.</p>
+    </div>
+  );
+}
